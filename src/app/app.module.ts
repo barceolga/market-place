58b4c8ecd2215fff7cd092c19926ac0c1ba1daa3
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-11-01 09:14:41 +0100

    Add store and reducer in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { basketActionReducer } from '../app/redux/basket.reducer';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({ basket: basketActionReducer}),
     AppRoutingModule
   ],
   providers: [],
