505d36bacd8b14cf0bbbd47095ed83db19269dd5
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-11-01 09:15:24 +0100

    Implement logic for the app (store and state)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from '../app/interfaces/item';
+import { select, Store } from '@ngrx/store';
+import { BasketAddAction, BasketRemoveAction } from '../app/redux/basket.action';
+import { AppState } from '../app/interfaces/app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'market-place';
+export class AppComponent implements OnInit{
+  items: Item[] = [];
+  currentBasekt: Item[];
+
+  constructor(
+    public store: Store<AppState>
+  ) {
+    this.store.pipe(select(state => state.basket))
+    .subscribe(basket => {
+      if (basket && this.items ) {
+        this.currentBasekt = basket.items
+      }
+    });
+  }
+
+  addItem(item: Item) {
+    this.store.dispatch(new BasketAddAction(item));
+  }
+
+  removeItem(item: Item) {
+    this.store.dispatch(new BasketRemoveAction(item));
+  }
+
+  ngOnInit() {
+    this.items.push({ id: '1', name: 'item1'}, {id: '2', name: 'item2'}, {id: '3', name: 'item3'});
+
+  }
 }
