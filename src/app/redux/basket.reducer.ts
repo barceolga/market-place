bcc70010d0e50a8e34eca0c2f7f6584336f14462
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-11-03 18:11:12 +0100

    Go back to switch cases in basket reducer, note homework for tomorrow

--- src/app/redux/basket.reducer.ts
+++ src/app/redux/basket.reducer.ts
@@ -1,38 +1,41 @@
 import { BasketState } from '../interfaces/basket.state';
-import { BasketActionAdd, BasketActionRemove } from './basket.action';
+import { BasketAction, BasketActionTypes} from './basket.action';
 import { State, createReducer, on } from '@ngrx/store';
-import { addItemToBasket } from '../app.component'
+import { addItemToBasket, sumPrice } from '../app.component'
 
 const initialBasketState: BasketState = {
     items: []
 }
+//TODO: create an interface sum with one property called sum with type Number
+// TODO: add sum as independent state in appState 
+// TODO: create a file called sum.action.ts to define the type of action
+// TODO: refactor reducer to make it work with two state values: basket and sum
+export function basketActionReducer( state: BasketState = initialBasketState, action: BasketAction): BasketState {
+    switch (action.type) {
+        case BasketActionTypes.ADD:
+            state.items.push(action.payload);
+            return {
+                items: state.items
+            };
+        case BasketActionTypes.REMOVE:
+            const index: number = state.items.indexOf(action.payload);
+            if (index != -1) {
+                state.items.splice(index, 1)
+            }
+            return {
+                items: state.items
+            };
+    }
+}
 
-// export function basketActionReducer( state: BasketState = intialBasketState, action: BasketAction): BasketState {
-//     switch (action.type) {
-//         case BasketActionTypes.ADD:
-//             state.items.push(action.payload);
-//             return {
-//                 items: state.items
-//             };
-//         case BasketActionTypes.REMOVE:
-//             const index: number = state.items.indexOf(action.payload);
-//             if (index != -1) {
-//                 state.items.splice(index, 1)
-//             }
-//             return {
-//                 items: state.items
-//             };
-//     }
-// }
-
-export const basketActionReducer = createReducer(
-    initialBasketState,
-    on(BasketActionAdd, (state, action) => ({
-        ...state,
-        items: { ...state.items, addItemToBasket(items, item)}
-    })),
-    on(BasketActionRemove, (state, action) => ({
-        ...state,
-        items: {}
-    }))
-)
+// export const basketActionReducer = createReducer(
+//     initialBasketState,
+//     on(BasketActionAdd, (state, action) => ({
+//         ...state,
+//         items: { ...state.items, addItemToBasket(items, item)}
+//     })),
+//     on(BasketActionRemove, (state, action) => ({
+//         ...state,
+//         items: {}
+//     }))
+// )
