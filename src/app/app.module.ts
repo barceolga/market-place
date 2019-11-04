911dd9c2f06435cff2b1e8d766533ba58af725ea
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-11-04 17:49:15 +0100

    Update app module (ngrx/effects(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { EffectsModule } from "@ngrx/effects";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { basketActionReducer } from '../app/redux/basket.reducer';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BasketEffects } from "./redux/basket.effects";
+import { basketActionReducer } from "../app/redux/basket.reducer";
+import { discountReducer } from "./redux/discount.reducer";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ basket: basketActionReducer}),
-    AppRoutingModule
+    StoreModule.forRoot({
+      basket: basketActionReducer,
+      discount: discountReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 50
+    }),
+    AppRoutingModule,
+    EffectsModule.forRoot([BasketEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
