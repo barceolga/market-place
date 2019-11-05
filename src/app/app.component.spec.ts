7993162229ff58b02dfad05b68af5e78304432cf
Olga Furmanowska <olgafurmanowska@gmail.com>
2019-11-05 18:01:12 +0100

    Update imports for testing, add 2 new test for component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,9 @@ import { AppComponent } from "./app.component";
 import { StoreModule } from "@ngrx/store";
 import { basketActionReducer } from "./redux/basket.reducer";
 import { discountReducer } from "./redux/discount.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { BasketEffects } from "./redux/basket.effects";
+import { take } from "rxjs/operators";
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
@@ -13,7 +16,8 @@ describe("AppComponent", () => {
         StoreModule.forRoot({
           basket: basketActionReducer,
           discount: discountReducer
-        })
+        }),
+        EffectsModule.forRoot([BasketEffects])
       ],
       declarations: [AppComponent]
     }).compileComponents();
@@ -48,10 +52,27 @@ describe("AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     const item = { id: "1", name: "test", price: 5 };
+    app.addItem(item);
+    app.basket$.pipe(take(1)).subscribe(basket => {
+      expect(basket.length).toEqual(1);
+    });
+
     app.removeItem(item);
     app.basket$.subscribe(basket => {
-      expect(basket.filter(i => i.id != item.id).length).toEqual(basket.length);
+      expect(basket.length).toEqual(0);
       done();
     });
-  })
+  });
+
+  it("should return 5 if the ham is put in the basket", done => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    const item = { id: "3", name: "ham", price: 5 };
+    app.addItem(item);
+    // We expect it to be working due to `discountOnHam` effect
+    app.discount$.subscribe(discount => {
+      expect(discount).toEqual(5);
+      done();
+    });
+  });
 });
